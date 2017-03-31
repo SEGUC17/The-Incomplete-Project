9c76d836af339ee17d093635089763508d6e8859
ahmedHathout <Ahmed.hathout@student.guc.edu.eg>
2017-03-31 19:04:31 +0200

    , viewBusinessPage

--- app/controllers/ownerController.js
+++ app/controllers/ownerController.js
@@ -1,11 +1,27 @@
 let Owner = require('../models/Owner');
+let BusinessPage = require('../models/BusinessPage');
 
 let ownerController = {
 
     viewBusinessPage:function(req, res) {
 
+        let businessPageId = req.session.data.businessPage;
+        BusinessPage.findOne(businessPageId, function(err, businessPage) {
+          if(err) {
+            res.send(err.message)
+            console.log(err);
+          }
+
+          else {
+            req.session.data = businessPage;
+            res.render('businessPage', {businessPage});
+          }
+
+        })
 
 
     }
+
+}
 
 module.exports = ownerController;
