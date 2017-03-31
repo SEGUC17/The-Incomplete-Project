9e845aea7871d81c545a709e9d5685bc2a37eb2c
IslamHamada <islamhamada2@yahoo.com>
2017-03-31 16:28:36 +0200

    view

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+var express = require('express');
+var routes = require('./app/routes');
+var bodyParser = require('body-parser');
 var app = express();
 var port = 8080;
+// var DB_URI = "mongodb://localhost:27017/Portsurf";
+
+
 app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({limit:'50mb',extended:false}));
+app.use(express.static(__dirname+ '/views'));
+
+// mongoose.connect(DB_URI);
+
 app.use(routes);
-app.listen(process.env.PORT||port);
+
+app.listen(port,function(){
+  console.log(port);
+});
