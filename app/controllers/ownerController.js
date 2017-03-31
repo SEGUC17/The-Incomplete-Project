cba83e80fce2483f518d2650b6731b88ee57fe6b
IslamHamada <islamhamada2@yahoo.com>
2017-03-31 19:23:30 +0200

    hey

--- app/controllers/ownerController.js
+++ app/controllers/ownerController.js
@@ -2,10 +2,27 @@ let Owner = require('../models/Owner');
 
 let ownerController = {
 
+    editProfile:function(req,res){
+      Owner.update({_id:session._id},{$set:{}},function(err,results){
+        if(err)
+          console.log(err.message);
+        else {
+          
+        }
+      });
+      students.update({_id:session._id},{$set: {portfolio:port._id}},function(err,results){
+        if(err)
+          console.log(err.message);
+        else{
+          session.portfolio=port._id;
+          res.render('user/user_home',{session});
+        }
+      });
+    },
     viewBusinessPage:function(req, res) {
 
 
 
     }
-
+}
 module.exports = ownerController;
