e7e7c3bed0ee395a90a7a8b6b4ab7a599483f2ca
Tarek <tarekshehab1997@gmail.com>
2017-04-02 14:22:37 +0200

    some edits on server.js

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-var express       = require('express');
-var mongoose      = require('mongoose');
-var morgan        = require('morgan');
-var bodyParser    = require('body-parser');
-var passport 	  = require('passport');
-var BasicStrategy = require('passport-http').Strategy;
+var express         = require('express');
+var mongoose        = require('mongoose');
+var morgan          = require('morgan');
+var bodyParser      = require('body-parser');
+var passport 	      = require('passport');
+var BasicStrategy   = require('passport-http');
 
 
-var routes = require('./app/routes');
+var routes          = require('./app/routes');
+var BusinessPage    = require('./app/models/BusinessPage');
+var Event           = require('./app/models/Event');
+var Owner           = require('./app/models/Owner');
+var Place           = require('./app/models/Place');
+var Profile         = require('./app/models/Profile');
+var RegisteredUser  = require('./app/models/RegisteredUser');
+var Trip            = require('./app/models/Trip');
 
 
-var app = express();
-var port = 8080;
+
+var app             = express();
+var port            = 8080;
 // var DB_URI = "mongodb://localhost:27017/Portsurf";
 
 mongoose.connect('mongodb://localhost:27017/INCOMPLETEPROJ');
@@ -48,7 +56,6 @@ passport.deserializeUser(function(id, done) {
 
 
 
-app.set('view engine','angular');
 app.use(express.static(__dirname+ '/views'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({limit:'50mb',extended:false}));
@@ -60,5 +67,5 @@ app.use(routes);
 
 
 app.listen(port,function(){
-  console.log(port);
+  console.log("magic is heppening in port: " + port);
 });
