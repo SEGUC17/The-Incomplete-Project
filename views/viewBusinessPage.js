99bec7a8f01f4ccc77a307a53c17f256153fbd80
IslamHamada <islamhamada2@yahoo.com>
2017-04-15 18:38:31 +0200

    bus

--- views/viewBusinessPage.js
+++ views/viewBusinessPage.js
@@ -7,25 +7,6 @@ angular.module('myApp', []).controller('viewBusinessPage', function($scope, $htt
       $scope.phoneNumber = response.data.businessPage.phoneNumber;
       $scope.addresses = response.data.businessPage.addresses;
       $scope.events = response.data.events;
-      // alert(response.data.events[0].event.price);
-      // $scope.images = response.data.images;
     });
 
-    // $scope.phoneNumber = ["123","456"]
-    // $scope.description = "hey2"
-    // $scope.profileImg = "http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/sign-check-icon.png"
-    //
-    // if($scope.event.mustPay){
-    //   $scope.event.payMsg = "it must be paid for"
-    // }else {
-    //   $scope.event.payMsg = "you don't have to pay now"
-    // }
-    //
-    // if($scope.event.mustPay){
-    //   $scope.event.repeatsMsg = "it must be paid for"
-    // }else {
-    //   $scope.event.repeatsMsg = "you don't have to pay now"
-    // }
-
-    // alert("hey");
 });
