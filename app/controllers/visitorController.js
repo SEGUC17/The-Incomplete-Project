35fdab256fe8ca1b45a3bc3d1bbe63563363e122
ahmadgafar <ahmad.gafar@student.guc.edu.eg>
2017-04-17 11:22:03 +0200

    working home page and search is slightly working

--- app/controllers/visitorController.js
+++ app/controllers/visitorController.js
@@ -53,7 +53,7 @@ let visitorController = {
             }
           }
 
-          res.json({"businessPagesResult" : businessPagesResult});
+          res.json({"SearchBusinessPagesResult" : businessPagesResult});
 
         }
   	});
