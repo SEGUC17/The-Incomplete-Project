2ce289f46393676711e2f07f1b8e43d7bd7bb9db
ahmedHathout <ahmed.hathout@student.guc.edu.eg>
2017-04-30 03:47:26 +0300

    Delete update_webapp_server.sh

--- update_webapp_server.sh
+++
@@ -1,7 +0,0 @@
-git pull origin master
-rm -rf node_modules/
-find ./ -type f -exec sed -i -e 's/http\/\/localhost:8080/htt
-p:\/\/34.210.7.210:8080/g' {} \;
-pm2 stop 0
-pm2 delete 0
-pm2 start server.js
